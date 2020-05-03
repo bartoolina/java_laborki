7920b4ca88cfa2e80fec05162189cc84dede25d3
bartool <bartoolina@gmail.com>
2020-05-03 22:50:48 +0200

    Zadanie 3 i 4: get your salary & give yourself a rise

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,10 +1,44 @@
 package com.company;
 
+import javafx.util.Pair;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Human {
     String firstName;
     String lastName;
-
+    public static final Double DEFAULT_SALARY = 0.0;
+    private Double salary = DEFAULT_SALARY;
     Animal pet;
     Car car;
+    private ArrayList<Pair<LocalDate, Double>> salaryHistory = new ArrayList<>();
+
+    public Human(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Double getSalary() {
+        for (var record : salaryHistory) {
+            System.out.println(record.getKey() + " " + record.getValue());
+        }
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary > 0.0) {
+            LocalDate now = LocalDate.now();
+            salaryHistory.add(new Pair<LocalDate, Double>(now, salary));
+
+            System.out.println("New value of salary has been added to salary history.");
+            System.out.println("Please pick up the annex to the contract form Mrs Hania.");
+            System.out.println("ZUS and US has been informed.");
+
+            this.salary = salary;
+        } else {
+            System.out.println("Cannot add a negative value");
+        }
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Human me = new Human();
+        Human me = new Human("Marcin", "Nowak");
 
         Animal dog = new Animal("dog");
         dog.name = "Burek";
@@ -11,5 +11,10 @@ public class Main {
         Car audi = new Car("Audi", "A3", 2006);
         me.car = audi;
 
+        System.out.println(me.getSalary());
+        me.setSalary(2000.0);
+        me.setSalary(2500.0);
+        me.setSalary(-1.0);
+        System.out.println(me.getSalary());
     }
 }
